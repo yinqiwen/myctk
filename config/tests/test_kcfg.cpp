b8476c44aa3b4d448e039f387fae4b75da730951
yinqiwen <yinqiwen@gmail.com>
2022-07-01 16:23:22 +0800

    add kcfg yaml impl

--- config/tests/test_kcfg.cpp
+++ config/tests/test_kcfg.cpp
@@ -2,6 +2,7 @@
 #include <kcfg_json.h>
 #include <vector>
 #include "kcfg.hpp"
+#include "kcfg_yaml.h"
 
 struct TestA {
   int64_t a = 11;
@@ -11,6 +12,8 @@ struct TestA {
   std::vector<int64_t> e = {1, 2, 0, 5};
 
   KCFG_DEFINE_FIELDS(a, b, c, d, e)
+
+  KCFG_YAML_DEFINE_FIELDS(a, b, c, d, e)
 };
 
 TEST(Searialize, JsonTest) {
@@ -22,4 +25,25 @@ TEST(Searialize, JsonTest) {
   s.clear();
   kcfg::WriteToJsonString(t, s, false, false);
   printf("%s\n", s.c_str());
+}
+
+TEST(Searialize, YamlTest) {
+  std::string content = R"(
+---
+ a: 101
+ b: 3.14
+ c: false
+ d: hello,world
+ e: [1,2,3,4,5]
+  )";
+  TestA t;
+  bool rc = kcfg::ParseFromYamlString(content, t);
+  EXPECT_EQ(true, rc);
+  EXPECT_EQ(101, t.a);
+  EXPECT_FLOAT_EQ(3.14, t.b);
+  EXPECT_EQ(false, t.c);
+  EXPECT_EQ("hello,world", t.d);
+  EXPECT_EQ(5, t.e.size());
+  EXPECT_EQ(1, t.e[0]);
+  EXPECT_EQ(5, t.e[4]);
 }
