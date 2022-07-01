902a1b7c4f815898a2247343146d8802076c59ae
yinqiwen <yinqiwen@gmail.com>
2022-07-01 16:23:51 +0800

    sync didagle fix

--- didagle/processor_dump.cpp
+++ didagle/processor_dump.cpp
@@ -1,5 +1,5 @@
 /*
- *Copyright (c) 2021, qiyingwang <qiyingwang@tencent.com>
+ *Copyright (c) 2021, yinqiwen <yinqiwen@gmail.com>
  *All rights reserved.
  *
  *Redistribution and use in source and binary forms, with or without
